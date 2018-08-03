04d1ed0b24eec2cf267e6e9fe32bea3162db92a4
Peter Pouliot <peter@pouliot.net>
2018-08-03 16:39:44 -0400

    updating

--- files/bin/create_centos_vm.sh
+++ files/bin/create_centos_vm.sh
@@ -7,7 +7,7 @@ virt-install \
         --connect=qemu:///system \
         --vcpus 2 \
         --ram 4096 \
-        --network bridge=br0 \
+        --network bridge=br0,model=virtio \
         --serial pty \
         --console pty,target_type=serial \
         --disk path=/var/lib/libvirt/images/$1.qcow2,format=qcow2,size=$2,bus=virtio \
@@ -15,6 +15,4 @@ virt-install \
         --location  'http://mirror.centos.org/centos-7/7/os/x86_64/' \
         --extra-args "
     console=ttyS0,115200n9 serial
-    ksdevice=eth0 ks=http://i.pxe.to/kickstart/centos-7.4.1708-x86_64.kickstart
-" \
-        --network network=default,model=virtio
+    ksdevice=eth0 ks=http://i.pxe.to/kickstart/centos-7.4.1708-x86_64.kickstart"
