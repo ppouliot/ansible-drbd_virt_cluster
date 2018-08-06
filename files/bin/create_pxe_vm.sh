2455422bf0daee97754f40effd5c1412f69c7cb4
Peter Pouliot <peter@pouliot.net>
2018-08-06 12:55:20 -0400

    updating

--- files/bin/create_pxe_vm.sh
+++ files/bin/create_pxe_vm.sh
@@ -7,15 +7,12 @@ virt-install \
         --connect=qemu:///system \
         --vcpus 2 \
         --ram 4096 \
-        --network bridge=br0 \
-        -- pxe \
+        --network bridge=br0,model=virtio \
+        --pxe \
+        --hvm \
+        --accelerate \
         --serial pty \
         --console pty,target_type=serial \
         --disk path=/var/lib/libvirt/images/$1.qcow2,format=qcow2,size=$2,bus=virtio \
-        --nographics \
-        --location  'http://archive.ubuntu.com/ubuntu/dists/bionic/main/installer-amd64' \
-        --extra-args "
-    console=ttyS0,115200n9 serial
-    preseed/url=http://i.pxe.to/preseed/ubuntu-18.04-amd64.preseed
-" \
-        --network network=default,model=virtio
+        --boot useserial=on \
+        --nographics
