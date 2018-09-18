f6d59a5aae8b6342cddc9de9071622e2bf35fb2f
Peter Pouliot <peter@pouliot.net>
2018-09-18 14:55:44 -0400

    script changes

--- files/bin/list_vms.sh
+++ files/bin/list_vms.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
+echo 
+echo "==================================================================="
+echo "= *** Cluster CIB View of Virtual Machines Running on Cluster *** ="
+echo "==================================================================="
+crm resource show |grep vm_
+echo "==================================================================="
+echo 
 echo "===================================================="
-echo "= Hypervisor: virt-cl-drbd-0"
+echo "= Hypervisor View On: virt-cl-drbd-0"
 echo "===================================================="
 echo
 virsh --connect qemu+ssh://virt-cl-drbd-0/system list --all
 echo "===================================================="
 echo
 echo "===================================================="
-echo "= Hypervisor: virt-cl-drbd-1"
+echo "= Hypervisor View On: virt-cl-drbd-1"
 echo "===================================================="
 echo
 virsh --connect qemu+ssh://virt-cl-drbd-1/system list --all
